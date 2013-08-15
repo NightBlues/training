b0714678bbd4faf95f7562c2746bf407b32602fd
Vadim Radovel <forbidengateway@gmail.com>
2013-08-16 00:51:05 +0400

    Started Binary Search Tree.

--- datastruct/tree.py
+++ datastruct/tree.py
@@ -1,8 +1,8 @@
 __author__ = 'nightblues'
 
-class BinaryTree():
 
-    def __init__(self, value = None):
+class BinaryTree():
+    def __init__(self, value=None):
         self.parent = None
         self.l_child = None
         self.r_child = None
@@ -12,13 +12,19 @@ class BinaryTree():
         if not isinstance(el, self.__class__):
             el = self.__class__(el)
         self.l_child = el
-        el.parent=self
+        el.parent = self
 
     def set_right_child(self, el):
         if not isinstance(el, self.__class__):
             el = self.__class__(el)
         self.r_child = el
-        el.parent=self
+        el.parent = self
+
+    def get_root(self):
+        root = self
+        while root.parent is not None:
+            root = root.parent
+        return root
 
     def isRoot(self):
         return self.parent is None
@@ -26,11 +32,14 @@ class BinaryTree():
     def isLeaf(self):
         return self.l_child is None and self.r_child is None
 
+    def isEmpty(self):
+        return self.isLeaf() and self.isRoot() and self.value is None
+
     def bfs(self):
         """breadth-first search"""
         heap = []
         heap.append(self)
-        while len(heap)>0:
+        while len(heap) > 0:
             if heap[0].l_child is not None:
                 heap.append(heap[0].l_child)
             if heap[0].r_child is not None:
@@ -52,3 +61,27 @@ class BinaryTree():
 
     def __repr__(self):
         return self.__unicode__()
+
+
+class BinarySearchTree(BinaryTree):
+    def insert(self, element, key=lambda k: k):
+        if not isinstance(element, self.__class__):
+            element = self.__class__(element)
+        el_parent_place = None
+        el_place = self.get_root()
+        if el_place.isEmpty():
+            el_place.value = element.value
+            return
+        while el_place is not None:
+            el_parent_place = el_place
+            if key(element.value) < key(el_place.value):
+                el_place = el_place.l_child
+            else:
+                el_place = el_place.r_child
+        element.parent = el_parent_place
+        if element.isRoot():
+            return
+        if key(element.value) < key(element.parent.value):
+            element.parent.set_left_child(element)
+        else:
+            element.parent.set_right_child(element)
--- test/tree.py
+++ test/tree.py
@@ -4,12 +4,18 @@ sys.path.append('..')
 from datastruct.tree import *
 from test.util import CompEl
 
-bt = BinaryTree()
-el = bt
-for i in range(0,10, 2):
-    el.set_left_child(CompEl(chr(i+97), i))
-    el.set_right_child(CompEl(chr(i+98), i+1))
-    el = el.l_child
+# bt = BinaryTree()
+# el = bt
+# for i in range(0,10, 2):
+#     el.set_left_child(CompEl(chr(i+97), i))
+#     el.set_right_child(CompEl(chr(i+98), i+1))
+#     el = el.l_child
+#
 
-for x in bt.bfs():
+bst = BinarySearchTree()
+src = [7, 6, 4, 8, 9, 3, 5, 0, 2, 1]
+for i in src:
+    bst.insert(CompEl(chr(i+97), i), lambda k:k.key)
+
+for x in bst.bfs():
     print(x)
